00cec4d0f787d2b6ced1aedb2644ea450750b21d
Erison Veshi <erisonve@gmail.com>
2024-04-10 22:43:25 +0200

    Initial Commit

---
+++ SunoAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SunoAnimation
+//
+//  Created by Erison Veshi on 10.4.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SunoAnimation/SunoAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunoAnimationApp.swift
+//  SunoAnimation
+//
+//  Created by Erison Veshi on 10.4.24.
+//
+
+import SwiftUI
+
+@main
+struct SunoAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
